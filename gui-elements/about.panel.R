75c58e627d2633743d2ad6d536c6bb1e610888df
Chris Park <cpar137@aucklanduni.ac.nz>
2015-02-11 12:40:57 +1300

    Add footer to "About" page only.

--- gui-elements/about.panel.R
+++ gui-elements/about.panel.R
@@ -11,7 +11,44 @@ about.panel = function() {
                      style = 'color:gray',
                      align = 'right')
                    )
-               )
+               ),
+        column(width = 12,
+               HTML(
+            '<div id = "wrapper">
+		<div id = "footer">
+		  <span style = "float:left;">
+                  <br> &nbsp;
+		    <a href = "https://www.stat.auckland.ac.nz/~wild/iNZight/">
+		      iNZight Project
+		    </a> |
+                    <a href = "https://github.com/iNZightVIT/Lite/">
+                      R Source Code
+                    </a> |
+		    <a href = "mailto:cpar137@aucklanduni.ac.nz?Subject=iNZight-Lite%20Feedback"
+		       target = "_top">
+		      Contact Us
+		    </a>
+                    <br><br>
+                      &nbsp; Copyright 2015 iNZight | All Rights Reserved
+		  </span>
+		  <span style = "float:right;">
+                    <a href = "http://new.censusatschool.org.nz/">
+		      <img src = "census_logo.png"/, height = 75>
+		    </a> &nbsp; &nbsp;
+                    <a href = "http://www.stats.govt.nz/">
+		      <img src = "stats_nz.png"/, height = 75>
+		    </a> &nbsp; &nbsp;
+                    <a href = "http://www.minedu.govt.nz/">
+		      <img src = "minedu_logo.png"/, height = 60, width = 170>
+		    </a> &nbsp; &nbsp;
+		    <a href = "http://stat.auckland.ac.nz">
+		      <!-- <img src = "statistics_logo.png", height = 75, width = 160> -->
+                      <img src = "uoa_logo.png", height = 65>
+                    </a> &nbsp;
+		  </span>
+		</div>
+	      </div>'
+               ))
     )
 }
 
