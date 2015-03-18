11bbe75d35b7ab7cc887e352c2e74a7ff8f7e7c0
Chris Park <cpar137@aucklanduni.ac.nz>
2015-03-18 15:58:22 +1300

    Add padding to text.

--- gui-elements/add.columns.panel.R
+++ gui-elements/add.columns.panel.R
@@ -4,11 +4,11 @@
 get.sidebar.add = function(){
     list(
         helpText("Paste in a comma seperated list or have all values on a new line to add
-as column. If the list is shorter than there are rows in the current data
-set, the list of values is repeated until length equals the number of rows
-in the data. If the list is longer it will be shortened to the number of
-rows in the data. To have all values on a new line is preferered.
-Therefore if there are commas inside your values use the second method."),
+                as column. If the list is shorter than there are rows in the current data
+                set, the list of values is repeated until length equals the number of rows
+                in the data. If the list is longer it will be shortened to the number of
+                rows in the data. To have all values on a new line is preferered.
+                Therefore if there are commas inside your values use the second method."),
         tags$textarea(id="new.column", rows=5, cols=35, ""),br(),
         actionButton(inputId="add_column",label="Add as column",),br(),br(),
         help.display('Add a column','add_columns',"gui-elements/notes/add.columns.md")
