773636c572942bc31c28c44c2134baa2db790e99
Chris Park <cpar137@aucklanduni.ac.nz>
2015-03-18 15:59:33 +1300

    Update column.pair.plot.panel.R

--- gui-elements/column.pair.plot.panel.R
+++ gui-elements/column.pair.plot.panel.R
@@ -11,7 +11,7 @@ get.pair.plot.sidebar =  function(){
   }
   list(helpText("Select a column from the first and second drop down menu to display a appropriate plot of the selected columns."),
        selectInput("select.column.plot1","Select Column 1",choices=choices1,multiple=F,selectize=T,selected=choices1[1]),
-       selectInput("select.column.plot2","Select Column 2",choices="",multiple=F,selectize=T),
+       selectInput("select.column.plot2","Select Column 2",choices=choices1[-1],,multiple=F,selectize=T,selected=choices1[-1][1]),
        br(),br(),help.display('Column pair plot','column_pair_plot',"gui-elements/notes/column.pair.plot.md"),br(),HTML(""))
 }
 
