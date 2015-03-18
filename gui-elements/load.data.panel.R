b977117c6ee5e5e87c3801b74892b29f64be1ea2
Chris Park <cpar137@aucklanduni.ac.nz>
2015-03-18 16:11:30 +1300

    Update load.data.panel.R

--- gui-elements/load.data.panel.R
+++ gui-elements/load.data.panel.R
@@ -5,8 +5,11 @@
 get.sidebar.load = function(){
   list(
     fileInput("files", "File data", multiple=F),
-    helpText("The button below will import the data set and will make it available to other users of this instance."),
-    br(),br(),
+    helpText("The button above will import a data set from a file and will make it
+             available to other users of this instance. Otherwise, paste a URL in
+             the text field below to import a file from a web resource. Press the
+             \"Import file\" button below to finalize your choice"),
+    br(),textInput(inputId="URLtext",label="URL"),br(),
     actionButton(inputId="import_set",label="Import file",),
     br(),br(),help.display('Load data','load_data',"gui-elements/notes/load.data.md")
   )
