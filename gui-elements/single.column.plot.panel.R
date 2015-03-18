60ff5d9419a3dea1bcc0732997edca5d499f204c
Chris Park <cpar137@aucklanduni.ac.nz>
2015-03-18 16:01:49 +1300

    Update single.column.plot.panel.R

--- gui-elements/single.column.plot.panel.R
+++ gui-elements/single.column.plot.panel.R
@@ -6,7 +6,7 @@ get.single.col.sidebar <- function(){
         choices2 = ""
     }
     list(helpText("Select a column from the dropdown menu to display a
-approproate plot of the selected column."),
+                  approproate plot of the selected column."),
          selectInput("select.column.plot", "Select Column", choices = choices1,
                      multiple = FALSE, selectize = TRUE,
                      selected = choices1[1]),
@@ -15,33 +15,20 @@ approproate plot of the selected column."),
          br(),
          HTML(""))
 }
+
 single.column.plot.panel =function(){
-    if(is.null(data)){
-        sidebarLayout(
-            sidebarPanel(get.single.col.sidebar()),
+  if(is.null(data)){
+    sidebarLayout(
+        sidebarPanel(get.single.col.sidebar()),
             mainPanel(h1("Please select or import a data set."))
-        )
+    )
     }else{
-        sidebarLayout(
-            sidebarPanel(get.single.col.sidebar()),
+    sidebarLayout(
+        sidebarPanel(get.single.col.sidebar()),
             mainPanel(plotOutput("column.plot"),
-                      fixedRow(column(width=8,offset=2,
-                                      div(class='player',
-                                          fixedRow(
-                                              column(width=1,offset=1,
-                                                     div(class="seper",actionButton(inputId="single.backward",label="",icon=icon("backward")))),
-                                              column(width=6,offset=1,
-                                                     sliderInput(inputId="single.play",label="",min=1,max=ncol(data),step=1,
-                                                                 animate=animationOptions(interval=500,loop=T,play=T),
-                                                                 width="100%",value=1,ticks=F)),
-                                        # column(width=1,offset=1,
-                                        # actionButton(inputId="single.play",label="",icon=icon("play"))),
-                                        # column(width=1,offset=0,
-                                        # actionButton(inputId="single.pause",label="",icon=icon("pause"))),
-                                              column(width=1,offset=1,
-                                                     div(class="seper",actionButton(inputId="single.forward",label="",icon=icon("forward"))))
-                                          ))))
-                      )
-        )
+                get.player(ID.backward="single.backward",ID.player="single.play",
+                           ID.forward="single.forward",maxi=ncol(data))
+            )
+    )   
     }
 }
