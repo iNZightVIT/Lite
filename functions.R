145eed0372633f17061917e376d1518ee16d8d5f
Shengwei Hu <shengwei.hu@auckland.ac.nz>
2017-05-14 15:40:01 +1200

    test version_read excel files_try2

--- functions.R
+++ functions.R
@@ -1312,8 +1312,7 @@ load.data = function(data_dir,fileID=NULL,path=NULL){
           temp = read.csv(full.name[indexes[1]],comment.char="#", na.strings = c("NULL","NA","N/A","#N/A","","<NA>"), stringsAsFactors=FALSE)
         }else if(tolower(ext)%in%"txt"){
           temp = read.delim(full.name[indexes[1]],comment.char="#", na.strings = c("NULL","NA","N/A","#N/A","","<NA>"))
-        }
-        else if(tolower(ext)%in%"xls"){
+        }else if(tolower(ext)%in%"xls"){
           temp = data.frame(read_xls(full.name[indexes[1]], na = c("NULL","NA","N/A","#N/A","","<NA>")))
         }else if(tolower(ext)%in%"xlsx"){
           temp = data.frame(read_xlsx(full.name[indexes[1]], na = c("NULL","NA","N/A","#N/A","","<NA>")))
