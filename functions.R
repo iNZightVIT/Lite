0b41dbfb253da421bfb86b63cb3d43ea380c9c38
Shengwei Hu <shengwei.hu@auckland.ac.nz>
2018-10-31 12:27:37 +1300

    public_fix some bugs

--- functions.R
+++ functions.R
@@ -1309,9 +1309,11 @@ load.data = function(data_dir,fileID=NULL,path=NULL){
         }else if(tolower(ext)%in%"txt"){
           temp = read.delim(full.name[indexes[1]],comment.char="#", na.strings = c("NULL","NA","N/A","#N/A","","<NA>"))
         }else if(tolower(ext)%in%"xls"){
-          temp = read.xlsx(full.name[indexes[1]], 1)
+#          temp = read.xlsx(full.name[indexes[1]], 1)
+          temp = read_excel(full.name[indexes[1]])
         }else if(tolower(ext)%in%"xlsx"){
-          temp = read.xlsx(full.name[indexes[1]], 1)
+#          temp = read.xlsx(full.name[indexes[1]], 1)
+          temp = read_excel(full.name[indexes[1]])
         }else if(tolower(ext)%in%"sas7bdat"){
           temp = read.sas7bdat(full.name[indexes[1]])
         }else if(tolower(ext)%in%"dta"){
