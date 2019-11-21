4b600f343a7edeaff53cb819262799932997483c
T0ngChen <tche929@aucklanduni.ac.nz>
2019-11-21 17:04:12 +1300

    tidy code history

--- panels/B1_ImportDataset/2_import.data.set.panel-server.R
+++ panels/B1_ImportDataset/2_import.data.set.panel-server.R
@@ -45,6 +45,7 @@ observeEvent(input$files, {
             if (inherits(y, "try-error")) x else c(y, "\n")
           }))
           code = paste0(code, collapse = "\n ")
+          print(gsub("(.*)\\).*","\\1)", code))
           code.save$variable = c(code.save$variable, list(c("\n", code, "\n")))
         }
         values$name.restore = temp.name
