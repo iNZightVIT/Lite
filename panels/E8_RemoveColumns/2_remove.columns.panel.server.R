79d79ac54e87f0bbb2e10cfb4aa7a25483450a9b
T0ngChen <tche929@aucklanduni.ac.nz>
2019-11-22 16:24:44 +1300

    add code history

--- panels/E8_RemoveColumns/2_remove.columns.panel.server.R
+++ panels/E8_RemoveColumns/2_remove.columns.panel.server.R
@@ -5,10 +5,14 @@ observe({
   if(!is.null(get.data.set())&&!is.null(input$rem_column)&&input$rem_column>0){
     isolate({
       if(length(which(colnames(get.data.set())%in%input$select.remove.column))>0){
-        temp = as.data.frame(get.data.set()[,-which(colnames(get.data.set())%in%input$select.remove.column)])
+        #temp = as.data.frame(get.data.set()[,-which(colnames(get.data.set())%in%input$select.remove.column)])
+        temp = iNZightTools::deleteVars(get.data.set(), input$select.remove.column)
         if(!is.null(temp)){
           updatePanel$datachanged = updatePanel$datachanged+1
           values$data.set = temp
+          ## code history
+          code = tidy_assign_pipe(gsub("get.data.set\\()", code.save$name, iNZightTools::code(values$data.set)))
+          code.save$variable = c(code.save$variable, list(c("\n", code, "\n")))
           if(ncol(get.data.set())==0){
             values$data.set = NULL
             updateSelectInput(session, inputId="select.remove.column",choices=c(""),selected="")
