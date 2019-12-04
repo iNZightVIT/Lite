1b08beb5bfacd0cd397054288ef32602a31a0150
T0ngChen <tche929@aucklanduni.ac.nz>
2019-12-04 18:59:25 +1300

    add code history

--- panels/D2_SortDataByVariables/2_sort.variables.server.R
+++ panels/D2_SortDataByVariables/2_sort.variables.server.R
@@ -2,49 +2,53 @@
 
 output$sort1_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "sort1",
-                label = NULL,
-                choices = c("", colnames(get.data.set())),
-                selected = input$sort1,
-                selectize = F)
-    
+  isolate({
+    selectInput(
+      inputId = "sort1",
+      label = NULL,
+      choices = c("", colnames(get.data.set())),
+      selected = input$sort1,
+      selectize = F
+    )
   })
 })
 
 output$sort2_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "sort2",
-                label = NULL,
-                choices = c("", colnames(get.data.set())),
-                selected = input$sort2,
-                selectize = F)
-    
+  isolate({
+    selectInput(
+      inputId = "sort2",
+      label = NULL,
+      choices = c("", colnames(get.data.set())),
+      selected = input$sort2,
+      selectize = F
+    )
   })
 })
 
 output$sort3_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "sort3",
-                label = NULL,
-                choices = c("", colnames(get.data.set())),
-                selected = input$sort3,
-                selectize = F)
-    
+  isolate({
+    selectInput(
+      inputId = "sort3",
+      label = NULL,
+      choices = c("", colnames(get.data.set())),
+      selected = input$sort3,
+      selectize = F
+    )
   })
 })
 
 output$sort4_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "sort4",
-                label = NULL,
-                choices = c("", colnames(get.data.set())),
-                selected = input$sort4,
-                selectize = F)
-    
+  isolate({
+    selectInput(
+      inputId = "sort4",
+      label = NULL,
+      choices = c("", colnames(get.data.set())),
+      selected = input$sort4,
+      selectize = F
+    )
   })
 })
 
@@ -53,73 +57,116 @@ output$sort4_panel = renderUI({
 observe({
   input$sort_vars
   isolate({
-#    if(!is.null(input$sort_vars) && input$sort_vars>0){
-#      indexes1 = grep("^sort[0-9]+$",names(input))
-#      vars = unlist(lapply(indexes1, function(i,nams){
-#        input[[nams[i]]]
-#      },names(input)))
-#      indexes2 = grep("^increasing[0-9]+$",names(input))
-#      sort.type = unlist(lapply(indexes2, function(i,nams){
-#        input[[nams[i]]]
-#      },names(input)))
-#      if(anyDuplicated(vars)){
-#        dups = which(duplicated(vars))
-#        vars = vars[-dups]
-#        sort.type =sort.type[-dups]
-#      }
-#      if("" %in% vars){
-#        empties = which(vars %in% "")
-#        vars = vars[-empties]
-#        sort.type =sort.type[-empties]
-#      }
-#      print(str(vars))
-#      print(vars)
-#      print(sort.type)
-      
-    vars = NULL; sort.type = NULL
-    if(!is.null(input$sort1) && input$sort1 != "") {
+    #    if(!is.null(input$sort_vars) && input$sort_vars>0){
+    #      indexes1 = grep("^sort[0-9]+$",names(input))
+    #      vars = unlist(lapply(indexes1, function(i,nams){
+    #        input[[nams[i]]]
+    #      },names(input)))
+    #      indexes2 = grep("^increasing[0-9]+$",names(input))
+    #      sort.type = unlist(lapply(indexes2, function(i,nams){
+    #        input[[nams[i]]]
+    #      },names(input)))
+    #      if(anyDuplicated(vars)){
+    #        dups = which(duplicated(vars))
+    #        vars = vars[-dups]
+    #        sort.type =sort.type[-dups]
+    #      }
+    #      if("" %in% vars){
+    #        empties = which(vars %in% "")
+    #        vars = vars[-empties]
+    #        sort.type =sort.type[-empties]
+    #      }
+    #      print(str(vars))
+    #      print(vars)
+    #      print(sort.type)
+    
+    vars = NULL
+    sort.type = NULL
+    if (!is.null(input$sort1) && input$sort1 != "") {
       vars = c(vars, input$sort1)
       sort.type = c(sort.type, ifelse(input$sort1_order == 1, TRUE, FALSE))
     }
-    if(!is.null(input$sort2) && input$sort2 != "") {
-      if(! input$sort2 %in% vars) {
+    if (!is.null(input$sort2) && input$sort2 != "") {
+      if (!input$sort2 %in% vars) {
         vars = c(vars, input$sort2)
         sort.type = c(sort.type, ifelse(input$sort2_order == 1, TRUE, FALSE))
       }
     }
-    if(!is.null(input$sort3) && input$sort3 != "") {
-      if(! input$sort3 %in% vars) {
+    if (!is.null(input$sort3) && input$sort3 != "") {
+      if (!input$sort3 %in% vars) {
         vars = c(vars, input$sort3)
         sort.type = c(sort.type, ifelse(input$sort3_order == 1, TRUE, FALSE))
       }
     }
-    if(!is.null(input$sort4) && input$sort4 != "") {
-      if(! input$sort4 %in% vars) {
+    if (!is.null(input$sort4) && input$sort4 != "") {
+      if (!input$sort4 %in% vars) {
         vars = c(vars, input$sort4)
         sort.type = c(sort.type, ifelse(input$sort4_order == 1, TRUE, FALSE))
       }
     }
-    
-    if(!is.null(vars)) {
-      temp = sort.data(vars, sort.type, get.data.set())
-      if(!is.null(temp)){
-        updatePanel$datachanged = updatePanel$datachanged+1
+    if (!is.null(vars)) {
+      temp = iNZightTools::sortVars(get.data.set(), vars, sort.type)
+      if (!is.null(temp)) {
+        ## save code
+        code.save$dataname = paste(code.save$name, "sorted", sep = ".")
+        code = tidy_assign_pipe(gsub(
+          "get.data.set\\()",
+          code.save$name,
+          iNZightTools::code(temp)
+        ))
+        code = do.call(paste, c(as.list(code), sep = ""))
+        code = do.call(c, lapply(code, function(x) {
+          y <- try({
+            formatR::tidy_source(
+              text = x,
+              width.cutoff = 80,
+              output = F,
+              indent = 4
+            )$text.tidy
+          }, silent = TRUE)
+          if (inherits(y, "try-error"))
+            x
+          else
+            c(y, "\n")
+        }))
+        code = c(paste0(code.save$dataname, " <- \n"), code)
+        code.save$variable = c(code.save$variable, list(c("\n", code)))
+        ## save data
+        updatePanel$datachanged = updatePanel$datachanged + 1
         values$data.set = temp
-        updateSelectInput(session,"sort1", selected = 0, choices = colnames(get.data.set()))
-        updateSelectInput(session,"sort2", selected = 0, choices = colnames(get.data.set()))
-        updateSelectInput(session,"sort3", selected = 0, choices = colnames(get.data.set()))
-        updateSelectInput(session,"sort4", selected = 0, choices = colnames(get.data.set()))
+        code.save$name = code.save$dataname
+        values$data.name = code.save$dataname
+        updateSelectInput(session,
+                          "sort1",
+                          selected = 0,
+                          choices = colnames(get.data.set()))
+        updateSelectInput(session,
+                          "sort2",
+                          selected = 0,
+                          choices = colnames(get.data.set()))
+        updateSelectInput(session,
+                          "sort3",
+                          selected = 0,
+                          choices = colnames(get.data.set()))
+        updateSelectInput(session,
+                          "sort4",
+                          selected = 0,
+                          choices = colnames(get.data.set()))
       }
     }
-    
-#    }
+    #    }
   })
 })
 
 output$sort.table = renderDataTable({
   input$sort_vars
   get.data.set()
-},options=list(lengthMenu = c(5, 30, 50), pageLength = 5, columns.defaultContent="NA",scrollX=T))
+}, options = list(
+  lengthMenu = c(5, 30, 50),
+  pageLength = 5,
+  columns.defaultContent = "NA",
+  scrollX = T
+))
 
 output$sort.variables = renderUI({
   sort.variables.panel()
