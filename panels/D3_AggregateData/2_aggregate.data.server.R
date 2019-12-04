3cc59a47a84c6ded2d240696e75cdcbad7dae0dc
T0ngChen <tche929@aucklanduni.ac.nz>
2019-12-04 19:05:07 +1300

    add code history

--- panels/D3_AggregateData/2_aggregate.data.server.R
+++ panels/D3_AggregateData/2_aggregate.data.server.R
@@ -2,36 +2,42 @@
 
 output$aggros1_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "aggros1",
-                label = NULL,
-                choices = c("", get.categorical.column.names(get.data.set())),
-                selected = input$aggros1,
-                selectize = F)
-
+  isolate({
+    selectInput(
+      inputId = "aggros1",
+      label = NULL,
+      choices = c("", get.categorical.column.names(get.data.set())),
+      selected = input$aggros1,
+      selectize = F
+    )
+    
   })
 })
 
 output$aggros2_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "aggros2",
-                label = NULL,
-                choices = c("", get.categorical.column.names(get.data.set())),
-                selected = input$aggros2,
-                selectize = F)
+  isolate({
+    selectInput(
+      inputId = "aggros2",
+      label = NULL,
+      choices = c("", get.categorical.column.names(get.data.set())),
+      selected = input$aggros2,
+      selectize = F
+    )
     
   })
 })
 
 output$aggros3_panel = renderUI({
   get.data.set()
-  isolate({    
-    selectInput(inputId = "aggros3",
-                label = NULL,
-                choices = c("", get.categorical.column.names(get.data.set())),
-                selected = input$aggros3,
-                selectize = F)
+  isolate({
+    selectInput(
+      inputId = "aggros3",
+      label = NULL,
+      choices = c("", get.categorical.column.names(get.data.set())),
+      selected = input$aggros3,
+      selectize = F
+    )
     
   })
 })
@@ -41,44 +47,89 @@ output$aggros3_panel = renderUI({
 observe({
   input$aggregate_vars
   isolate({
-    if(!is.null(input$aggregate_vars) && input$aggregate_vars > 0){
-#      vars = input$aggros
-#      rem = which(vars %in% "")
-#      if(length(rem)>0){
-#        vars = vars[-rem]
-#      }
-      
+    if (!is.null(input$aggregate_vars) && input$aggregate_vars > 0) {
+      #      vars = input$aggros
+      #      rem = which(vars %in% "")
+      #      if(length(rem)>0){
+      #        vars = vars[-rem]
+      #      }
       vars = NULL
-      if(!is.null(input$aggros1) && input$aggros1 != "")
+      if (!is.null(input$aggros1) && input$aggros1 != "")
         vars = c(vars, input$aggros1)
-      if(!is.null(input$aggros2) && input$aggros2 != "")
+      if (!is.null(input$aggros2) && input$aggros2 != "")
         vars = c(vars, input$aggros2)
-      if(!is.null(input$aggros3) && input$aggros3 != "")
+      if (!is.null(input$aggros3) && input$aggros3 != "")
         vars = c(vars, input$aggros3)
       
-      if(!is.null(input$aggregate.method) && length(input$aggregate.method) > 0) {
+      if (!is.null(input$aggregate.method) &&
+          length(input$aggregate.method) > 0) {
         methods = input$aggregate.method
         methods = tolower(methods)
-        if("iqr" %in% methods)
+        if ("iqr" %in% methods)
           methods[methods == "iqr"] = "IQR"
       }
       
-#      rem  = which(methods %in% "")
-#      if(length(rem) > 0){
-#        methods = methods[-rem]
-#      }
-#      print(vars)
-#      print(methods)
-#      print(get.data.set())
-      if(length(vars) > 0 & length(methods) > 0 & !is.null(get.data.set())) {
-        temp = aggregate.data(aggregate.over = unique(vars), methods = methods, dafr = get.data.set())
-        if(!is.null(temp)){
-          updatePanel$datachanged = updatePanel$datachanged+1
-          values$data.set = unlist.data.frame(temp)          
-          updateSelectInput(session,"aggros1", selected = 0, choices = get.categorical.column.names(get.data.set()))
-          updateSelectInput(session,"aggros2", selected = 0, choices = get.categorical.column.names(get.data.set()))
-          updateSelectInput(session,"aggros3", selected = 0, choices = get.categorical.column.names(get.data.set()))
-          updateSelectInput(session,"aggregate.method", selected = 0)
+      #      rem  = which(methods %in% "")
+      #      if(length(rem) > 0){
+      #        methods = methods[-rem]
+      #      }
+      #      print(vars)
+      #      print(methods)
+      #      print(get.data.set())
+      if (length(vars) > 0 &
+          length(methods) > 0 & !is.null(get.data.set())) {
+        temp = iNZightTools::aggregateData(get.data.set(),
+                                           vars = unique(vars),
+                                           summaries = methods)
+        if (!is.null(temp)) {
+          ## save code
+          code.save$dataname = paste(code.save$name, "aggregated", sep = ".")
+          code = tidy_assign_pipe(gsub(
+            "get.data.set\\()",
+            code.save$name,
+            iNZightTools::code(temp)
+          ))
+          code = do.call(paste, c(as.list(code), sep = ""))
+          code = do.call(c, lapply(code, function(x) {
+            y <- try({
+              formatR::tidy_source(
+                text = x,
+                width.cutoff = 80,
+                output = F,
+                indent = 4
+              )$text.tidy
+            }, silent = TRUE)
+            if (inherits(y, "try-error"))
+              x
+            else
+              c(y, "\n")
+          }))
+          code = c(paste0(code.save$dataname, " <- \n"), code)
+          code.save$variable = c(code.save$variable, list(c("\n", code)))
+          ## save data
+          updatePanel$datachanged = updatePanel$datachanged + 1
+          values$data.set = temp
+          code.save$name = code.save$dataname
+          values$data.name = code.save$dataname
+          updateSelectInput(
+            session,
+            "aggros1",
+            selected = 0,
+            choices = get.categorical.column.names(get.data.set())
+          )
+          updateSelectInput(
+            session,
+            "aggros2",
+            selected = 0,
+            choices = get.categorical.column.names(get.data.set())
+          )
+          updateSelectInput(
+            session,
+            "aggros3",
+            selected = 0,
+            choices = get.categorical.column.names(get.data.set())
+          )
+          updateSelectInput(session, "aggregate.method", selected = 0)
         }
       }
     }
@@ -91,17 +142,9 @@ output$aggregate.variable = renderUI({
 
 output$aggregate.table = renderDataTable({
   get.data.set()
-},options=list(lengthMenu = c(5, 30, 50), pageLength = 5, 
-               columns.defaultContent = "NA",scrollX = T))
-
-
-
-
-
-
-
-
-
-
-
-
+}, options = list(
+  lengthMenu = c(5, 30, 50),
+  pageLength = 5,
+  columns.defaultContent = "NA",
+  scrollX = T
+))
