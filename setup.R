c33d23cc2236db87e57e07875fe966f7e56caeff
Tom Elliott <tomelliottnz@gmail.com>
2025-01-08 10:47:36 +1300

    ttrigger image rebuild

--- setup.R
+++ setup.R
@@ -19,7 +19,7 @@ pkgs <-
         "iNZightVIT/iNZightTS@legacy",
         "iNZightVIT/iNZightRegression@1.3.3",
         "iNZightVIT/iNZightMR@2.2.7",
-        # specific version:
+        # specific version...
         "iNZightVIT/iNZightPlots@v2.14",
         "cran/maptools",
         "cran/rgeos",
