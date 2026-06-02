a0c3a60b3c1971f3de778ad3c6376ec24bdcd4c8
Charco Hui <charco.hui@auckland.ac.nz>
2026-06-03 10:12:58 +1200

    Features/manual ts (#456)
    * add manual ts options
    
    * format

--- panels/F2_TimeSeries/1_timeseries-panel-ui.R
+++ panels/F2_TimeSeries/1_timeseries-panel-ui.R
@@ -7,7 +7,6 @@ ts.help <- function() {
 }
 
 
-
 TS.sidebarPanel <- function(data.set) {
   if (is.null(data.set)) {
     stop("Please select a data set!")
@@ -15,12 +14,142 @@ TS.sidebarPanel <- function(data.set) {
 
   sidebarPanelUI <- list(
     hr(),
-    
     h5(strong("Time Information: ")),
-    uiOutput("tsui_time_select"),
-    uiOutput("tsui_key_select"),
+    tabsetPanel(
+      id = "tsui_time_info_mode",
+      tabPanel(
+        "Select time variable",
+        uiOutput("tsui_time_select"),
+        uiOutput("tsui_key_select")
+      ),
+      tabPanel(
+        "Provide time manually",
+        div(
+          class = "tsui-manual-time",
+          style = "padding-top: 14px;",
+          div(
+            style = "margin-bottom: 16px;",
+            fixedRow(
+              column(
+                4,
+                tags$label(class = "control-label", `for` = "tsui_period", "Period :")
+              ),
+              column(
+                8,
+                selectInput(
+                  inputId = "tsui_period",
+                  label = NULL,
+                  choices = c("Year", "Week", "Day"),
+                  selected = "Year"
+                )
+              )
+            )
+          ),
+          div(
+            style = "margin-bottom: 16px;",
+            fixedRow(
+              column(
+                4,
+                tags$label(class = "control-label", "Frequency* :")
+              ),
+              column(
+                8,
+                div(
+                  style = "margin-bottom: -10px;",
+                  fixedRow(
+                    column(6, uiOutput("tsui_manual_freq")),
+                    column(
+                      6,
+                      numericInput(
+                        inputId = "tsui_time_freq_num",
+                        label = NULL,
+                        value = 1,
+                        min = 1,
+                        step = 1
+                      )
+                    )
+                  )
+                ),
+                tags$p(
+                  style = "margin: 2px 0 0 0; font-size: 12px;",
+                  "*How many observations per period?"
+                )
+              )
+            )
+          ),
+          div(
+            style = "margin-bottom: 22px;",
+            fixedRow(
+              column(
+                4,
+                tags$label(
+                  class = "control-label",
+                  `for` = "tsui_time_start_period",
+                  "Start date :"
+                )
+              ),
+              column(
+                4,
+                div(
+                  style = "margin-bottom: -10px;",
+                  numericInput(
+                    inputId = "tsui_time_start_period",
+                    label = NULL,
+                    value = 1,
+                    min = 1,
+                    step = 1
+                  )
+                )
+              ),
+              column(
+                4,
+                div(
+                  style = "margin-bottom: -10px;",
+                  numericInput(
+                    inputId = "tsui_time_start_season",
+                    label = NULL,
+                    value = 1,
+                    min = 1,
+                    step = 1
+                  )
+                )
+              )
+            ),
+            div(
+              style = "margin-top: 2px;",
+              fixedRow(
+                column(4),
+                column(
+                  4,
+                  div(
+                    style = "font-size: 12px; line-height: 1.2;",
+                    textOutput("tsui_start_lbl1")
+                  )
+                ),
+                column(
+                  4,
+                  div(
+                    style = "font-size: 12px; line-height: 1.2;",
+                    textOutput("tsui_start_lbl2")
+                  )
+                )
+              )
+            )
+          ),
+          div(
+            style = "width: 100%; margin-top: 6px; text-align: right;",
+            div(
+              style = "display: inline-block;",
+              actionButton(
+                inputId = "tsui_manual_submit",
+                label = "Apply Settings"
+              )
+            )
+          )
+        )
+      )
+    ),
     hr(),
-    
     h5(strong("Choose variables:")),
     radioButtons(
       inputId = "tsui_choose_var_type",
@@ -46,11 +175,9 @@ TS.sidebarPanel <- function(data.set) {
       inline = T
     ),
     hr(),
-    
     h5(strong("Plot type:")),
     uiOutput("tsui_time_plot_info"),
     hr(),
-    
     h5(strong("Plot settings:")),
     checkboxInput("tsui_smoother", label = "Show smoother"),
     checkboxInput("tsui_seasonally_adjusted", label = "Seasonally adjust series"),
@@ -68,7 +195,6 @@ TS.sidebarPanel <- function(data.set) {
 }
 
 
-
 ### --------------###
 ###  Main Panel  ###
 ### --------------###
@@ -82,12 +208,11 @@ TS.mainPanel <- function() {
   ##    -  Section 3: Multiple Series Plots
   ##
   ##  Note the use of "br()" (= line break) for vertical spacing.
-  
+
   mainPanelUI <- list(
     uiOutput("tsui_main")
   )
 }
-
 
 
 ### ------------------###
@@ -114,16 +239,3 @@ TS.panel.ui <- function(data.set) {
     }
   )
 }
-
-###  Time information panel - conditional on the type of dataset.
-# output$time_info <- renderUI({
-#   radioButtons(
-#     inputId = "time_info",
-#     label = "Time Information: ",
-#     choices = c(
-#       "Select time variable" = 1,
-#       "Provide time manually" = 2
-#     )
-#   )
-# })
-
